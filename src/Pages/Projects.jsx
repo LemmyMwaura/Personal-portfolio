5646c3824f9902235226033a653b29e13aa58854
Lemmy Mwaura <lemminton@gmail.com>
2022-07-19 15:49:15 +0300

    feat: sets up projects component

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -6,8 +6,16 @@ import { useLocation } from "react-router-dom"
 import Transition from "../Components/Transition"
 import useLocoScroll from "../Hooks/useLocoScroll"
 
+//utility & images
+import projects from "../utils/projects"
+import sudokuImage from "../Assets/Images/Sudoku.png"
+import NetflixImage from "../Assets/Images/NetflixClone.png"
+
+const images = [sudokuImage, NetflixImage]
+
 const Projects = () => {
   const showPageData = useSelector(({ showPage }) => showPage.show)
+  const [activeProject, setActiveProject] = useState(1)
   const [showTransition, setShowTransition] = useState(false)
   const location = useLocation()
   const projectsRef = useRef(null)
@@ -38,16 +46,52 @@ const Projects = () => {
     <div>
       {showTransition && <Transition pagename="Projects" />}
       {showPageData && (
-        <>
-          <div
-            ref={projectsRef}
-            data-scroll-section
-            className="projects-container data-scroll-section"
-          >
-            Projects
+        <section
+          ref={projectsRef}
+          data-scroll-section
+          className="projects-container data-scroll-section"
+        >
+          <div className="projects-wrapper">
+            <div className="project-counter">
+              <span>{activeProject}</span>
+              <span className="divider" />
+              <span>{projects.length}</span>
+            </div>
+            {projects.map((project) => {
+              return <ProjectItem project={project} key={project.id} />
+            })}
           </div>
-        </>
+        </section>
       )}
+    </div>
+  )
+}
+
+const ProjectItem = ({ project }) => {
+  return (
+    <div className="project-wrapper">
+      <div />
+      <div className="project-item">
+        <div className="project-item-info">
+          <h3 className="project-info-title">{project.name}</h3>
+          <p className="project-info-desc">{project.description}</p>
+          <a
+            className="project-info-link"
+            target="_blank"
+            href={project.liveLink}
+          >
+            Link
+          </a>
+        </div>
+        <div
+          data-scroll
+          className="project-item-image"
+          style={{
+            backgroundImage: `url(${images[project.id]})`,
+          }}
+        ></div>
+      </div>
+      <div />
     </div>
   )
 }
